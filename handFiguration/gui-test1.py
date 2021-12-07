7611fad5bc4e036ad8f8089e6cce3d0e82710ae9
cloudy-1014 <cloudy838@icloud.com>
2021-12-07 18:58:35 +0900

    test modified

--- handFiguration/gui-test1.py
+++ handFiguration/gui-test1.py
@@ -37,6 +37,7 @@ hand_gesture = {
 input_gesture = 0
 input_gesture_switch = 0
 delay_time = 0
+cam = None
 
 #MediaPipe hands model
 mp_hands = mp.solutions.hands
@@ -52,85 +53,37 @@ knn.train(angle, cv2.ml.ROW_SAMPLE, label)
 
 def realcam():
     class camcamcam(tk.Frame):
-            def camThread():
-                cam = cv2.VideoCapture(0) #캠켜기
-                cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  #캠크기 조절
-                cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) #캠크기 조절
-                Frame = None
+        global cam
+        def camThread():
+            cam = cv2.VideoCapture(0) #캠켜기
+            cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  #캠크기 조절
+            cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) #캠크기 조절
+            Frame = None
 
-                while cam.isOpened():   #카메라가 열려있으면..
-                    success, image = cam.read() #한 프레임 씩 읽어옴
-                    if not success: #success 못하면 다음 프레임으로..?
-                        continue
-                    #success하면 go
-                    
-                    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB) #이미지 전처리(색상 형식 변경 & 이미지 한번 뒤집기)
-                    results = hands.process(image)  #전처리 및 모델 추론을 함께 실행..
-                    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) #출력을 위해 다시 색상 형식 바꿔주기
-                    image = PIL.Image.fromarray(image)
-                    image = PIL.ImageTk.PhotoImage(image)
+            while cam.isOpened():   #카메라가 열려있으면..
+                success, image = cam.read() #한 프레임 씩 읽어옴
+                if not success: #success 못하면 다음 프레임으로..?
+                    continue
+                #success하면 go
+                
+                image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB) #이미지 전처리(색상 형식 변경 & 이미지 한번 뒤집기)
+                results = hands.process(image)  #전처리 및 모델 추론을 함께 실행..
+                image = PIL.Image.fromarray(image)
+                image = PIL.ImageTk.PhotoImage(image)
 
-                    if Frame is None:
-                        Frame = tk.Label(image=image)
-                        Frame.image = image
-                        Frame.pack(side="top")
-                    else:
-                        Frame.configure(image=image)
-                        Frame.image = image
+                if Frame is None:
+                    Frame = tk.Label(image=image)
+                    Frame.image = image
+                    Frame.pack(side="top")
+                else:
+                    Frame.configure(image=image)
+                    Frame.image = image
 
-                    if results.multi_hand_landmarks:    #위 전처리를 통해 손이 인식 되면 참이됨
-                        for hand_landmarks in results.multi_hand_landmarks: #손 여러개 대비?? 예외처리 방지? with 써야되나?
-                            joint = np.zeros((21, 3))   #joint -> 빨간 점. 포인트 21개, xyz 3개. 생성
-                            for j, lm in enumerate(hand_landmarks.landmark):
-                                joint[j] = [lm.x, lm.y, lm.z]   #값 입력
-                                
-                            #joint 인덱스끼리 빼줘서 뼈대의 벡터 구하기(Fig 3의 형태)
-                            v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19],:] # Parent joint
-                            v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],:] # Child joint
-                            v = v2 - v1 # [20,3]
-                            #벡터의 길이로.. Normalize v?
-                            v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
-                            
-                            # Get angle using arcos of dot product
-                            angle = np.arccos(np.einsum('nt,nt->n',
-                                v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:],
-                                v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
-                                
-                            angle = np.degrees(angle) # Convert radian to degree
-                            
-                            # Inference gesture / 데이터 바꿔주고 정리..
-                            data = np.array([angle], dtype=np.float32)
-                            ret, results, neighbours, dist = knn.findNearest(data, 3)
-                            idx = int(results[0][0])
-                            
+        if __name__ == '__main__':
 
-                #            cv2.putText(image, text = hand_gesture[idx].upper(), org=(20, 60), fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 2, color = 255, thickness = 3)
-                            #손 가장 아랫부분에 제스쳐 이름 출력
-                            org = (int(hand_landmarks.landmark[0].x * image.shape[1]), int(hand_landmarks.landmark[0].y * image.shape[0]))
-                            cv2.putText(image, text=hand_gesture[idx].upper(), org=(org[0], org[1] + 30), fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 1, color=255, thickness = 2)
-
-                            mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS) #마디마디에 그려주는
-                            
-                            #손모양이 맞는지 아닌지 맞춰보기
-                            if input_gesture_switch:
-                                if idx == input_gesture:
-                                    delay_time += 1
-                                    if delay_time > 15:
-                                        input_gesture_switch = 0
-                                        delay_time = 0
-                                        print("okok")
-                    
-                    if input_gesture_switch == 1:
-                        cv2.putText(image, text = hand_gesture[input_gesture].upper(), org = (10,30), fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 1, color = 255, thickness = 2)
-                    
-                    cv2.waitKey(1)
-                cam.release()
-
-            if __name__ == '__main__':
-
-                    thread_img = threading.Thread(target=camThread, args=())
-                    thread_img.daemon = True
-                    thread_img.start()
+                thread_img = threading.Thread(target=camThread, args=())
+                thread_img.daemon = True
+                thread_img.start()
 
 class SampleApp(tk.Tk):
     def __init__(self):
